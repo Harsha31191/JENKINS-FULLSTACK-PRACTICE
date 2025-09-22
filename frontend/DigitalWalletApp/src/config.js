f704f5ba7621394307658de14cc834282f427ffb
Harsha31191 <harshavardhan8719@gmail.com>
2025-09-22 18:15:18 +0530

    version 2.0

--- frontend/DigitalWalletApp/src/config.js
+++ frontend/DigitalWalletApp/src/config.js
@@ -1,6 +1,6 @@
 // Backend API configuration
 const config = {
-  backendUrl: "http://localhost:8082" // Change this to your backend URL
+  backendUrl: "http://localhost:9090/springbootdigitalwalletapi" // Change this to your backend URL
 };
 
 export default config;
